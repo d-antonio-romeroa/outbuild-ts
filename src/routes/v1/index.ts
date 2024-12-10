15d51e734e225b4c49ab860428c94a1d34df433c
DANIEL ROMERO <danielromero@MacBook-Pro-de-DANIEL.local>
2024-12-10 20:31:34 -0300

    updated code docs

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -41,48 +41,21 @@ apiV1Router.use(validateProtectedRoute);
 
 const userRouter = express.Router();
 
-/**
- * @swagger
- * /api/v1/user/{userId}/schedules:
- *   post:
- *     summary: Create new schedule for an user.
- *     description: Create a new schedule with request payload data authenticated user id.
- *     tags: [Schedule]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - name
- *               - url
- *             properties:
- *               name:
- *                 type: string
- *                 description: Construction schedule name.
- *               url:
- *                 type: string
- *                 description: URL of an image with the construction objective (e.g., a building).
- *     responses:
- *       201:
- *         description: Schedule created successfully.
- *       400:
- *         description: Request is not valid or has missing data.
- *       500:
- *         description: Server internal error.
- *     security:
- *       - required: true
- *       - bearerAuth: ['1234']
- */
+// get schedule by id with nested activities
 userRouter.get('/:userId/schedules/:scheduleId', schedulesController.getByIdWithActivities as any)
 
+// create a new schedule with user id
 userRouter.post('/:userId/schedules', schedulesController.create as any)
 
+// add one activity to schedule
 userRouter.post('/:userId/schedules/:scheduleId/activities', activitiesController.create as any)
 
+// add many activity to schedule
 userRouter.post('/:userId/schedules/:scheduleId/activities/bulk', activitiesController.bulkCreate as any)
 
+// add many activity to schedule using queues
 userRouter.post('/:userId/schedules/:scheduleId/activities/bulk-queue', activitiesController.bulkCreateWithQueue as any)
+
+apiV1Router.use('/users', userRouter);
 
 export default apiV1Router;
